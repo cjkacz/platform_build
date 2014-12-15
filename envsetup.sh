0f66130f111ee16cae94712e7645eee675816f72
Donald Hoskins <grommish@gmail.com>
2014-12-14 22:00:15 -0500

    Add Garbage Collection to mka
    Change-Id: I85b2b53efbe6c0b1fc84ecca2462ded039dc4d7c

--- envsetup.sh
+++ envsetup.sh
@@ -1473,6 +1473,13 @@ function godir () {
 }
 
 function mka() {
+    ## Clean Up the Garbage
+    echo "Removing Previous Builds for $OUT"
+    rm -rf $OUT/system/build.prop
+    rm -rf $OUT/OCT-M*.zip*
+    rm -rf $OUT/to_*ota*.zip
+    echo "Clean Up Complete! Time to Make it Dirty"
+
     case `uname -s` in
         Darwin)
             make -j `sysctl hw.ncpu|cut -d" " -f2` "$@"
